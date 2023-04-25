84dfe8bb47bd816842457425ada95699c3c45d71
Abra19 <evabrarova@gmail.com>
2023-04-26 00:40:59 +0300

    external url

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.RENDER_EXTERNAL_URL || process.env.URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export default axiosInstance;
